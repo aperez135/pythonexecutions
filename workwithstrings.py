21d3e8abc03f0fe7d716f8b7693e9447f6203780
April Perez <87209554+aperez135@users.noreply.github.com>
2024-05-01 13:32:34 -0700

    Find the index

---
+++ workwithstrings.py
@@ -0,0 +1,10 @@
+
+
+
+# Find the index where the substring 192.168.243.140 starts within the string
+# contained in variable 
+
+ip_addresses = "192.168.140.81, 192.168.109.50, 192.168.243.140"
+ip_addresses_index = "192.168.140.81, 192.168.109.50, 192.168.243.140".index("192.168.243.140")
+print(ip_addresses_index)
+
